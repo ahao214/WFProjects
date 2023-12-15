a6adb09159bff2974ac9e7976f4db458c8c8b9f6
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-15 16:07:25 +0800

    登录页面

--- JokerHotelManager/HotelManagerUI/FrmLogin.Designer.cs
+++ JokerHotelManager/HotelManagerUI/FrmLogin.Designer.cs
@@ -120,6 +120,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Turquoise;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.ClientSize = new System.Drawing.Size(637, 426);
             this.Controls.Add(this.label4);
@@ -134,7 +135,7 @@
             this.DoubleBuffered = true;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FrmLogin";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Load += new System.EventHandler(this.FrmLogin_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
