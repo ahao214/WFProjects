213eb47f3f4c83797466d2f117ff4546d964fe36
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-15 14:49:19 +0800

    add 图片资源

--- JokerHotelManager/HotelManagerUI/FrmLogin.Designer.cs
+++ JokerHotelManager/HotelManagerUI/FrmLogin.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace HotelManagerUI
 {
-    partial class Form1
+    partial class FrmLogin
     {
         /// <summary>
         /// 必需的设计器变量。
@@ -28,10 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmLogin));
+            this.SuspendLayout();
+            // 
+            // FrmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(578, 444);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
