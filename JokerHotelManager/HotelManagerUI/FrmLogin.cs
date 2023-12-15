8d4ea424b1b1ff8ad8b53e9aaf5aef9c695632e2
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-15 16:44:30 +0800

    login

--- JokerHotelManager/HotelManagerUI/FrmLogin.cs
+++ JokerHotelManager/HotelManagerUI/FrmLogin.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Sunny.UI;
@@ -32,6 +33,23 @@ namespace HotelManagerUI
         private void FrmLogin_Load(object sender, EventArgs e)
         {
             Text = "欢迎使用酒店管理系统 当前时间是:" + DateTime.Now.ToString("yyyy年MM月dd日 HH:mm:ss");
+        }
+
+        #endregion
+
+        #region
+
+        private void TimeStart()
+        {
+            // 定义一个线程
+            Thread thread = new Thread (new ThreadStart(GetTimes));
+            thread.Start();     // 让线程执行
+        }
+
+        private void GetTimes()
+        {
+
+
         }
 
         #endregion
